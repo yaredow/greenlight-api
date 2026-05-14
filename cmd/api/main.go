42f7604d294292134cf0441013dd378b9510de4e
yaredow <yaredyilma11@gmail.com>
2026-05-14 21:27:26 +0300

    feat: impliment rate limiting with client ip

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,6 +25,12 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  time.Duration
 	}
+
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -39,6 +45,11 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	// rate limiter configuration flags
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Rate limiter enabled")
 
 	flag.Parse()
 
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.26.2
 require github.com/julienschmidt/httprouter v1.3.0
 
 require github.com/lib/pq v1.12.3
+
+require golang.org/x/time v0.15.0
